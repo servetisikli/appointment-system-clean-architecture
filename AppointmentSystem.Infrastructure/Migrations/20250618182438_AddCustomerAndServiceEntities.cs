9746fb03385f077794e060fe764fa92312fb5055
servetisikli <servetisikli@gmail.com>
2025-06-18 20:29:56 +0200

    edited

---
+++ AppointmentSystem.Infrastructure/Migrations/20250618182438_AddCustomerAndServiceEntities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppointmentSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerAndServiceEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
